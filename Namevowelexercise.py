f12e878d2841aff46ef30b43e7a36bdf031cf55e
Shaun <Shaunwoodier@live.co.uk>
2021-09-07 14:34:56 +0100

    Exercise complete

--- Namevowelexercise.py
+++ Namevowelexercise.py
@@ -1,65 +1,72 @@
-def getname():
-    
-    while True:
-        firstname = input("Enter first name: ").upper()
-        if firstname.isdigit() == True:
-            print("Your name can't be numbers!")
-        else:
-            break
+# name compatibility calculator
+# two names as input
+# take name length
+#loop that counts all vowels
+# validate name isnt numbers
+# do maths
+# write at least two procedures/methods
 
+
+
+
+def getname(name):
     
     while True:
-        secondname = input("Enter second name: ").upper()  
-        if secondname.isdigit() == True:
-            print("Your name can't be numbers!")
+        if name.isdigit() == True:
+            print("A name can't be numbers!")
+            name = input("Enter valid name Name: ").upper()
         else:
             break
      
-    return firstname,secondname      
+    return name   
     
 
 
-
-def checkforvowels(nameone,nametwo):
+def checkforvowels(name):
  
     vowels = ["A","E","I","O","U"]
-    totalone = 0
-    totaltwo = 0
-
-    for i in nameone:
+    total = 0
+    
+    for i in name:
         for c in vowels:
             if i == c:
-                totalone = totalone + 3
+                total = total + 10
     
-    for i in nametwo:
-        for c in vowels:
-            if i == c:
-                totaltwo = totaltwo + 3
+ 
 
-    return totalone, totaltwo
+    return total
 
-def findlength(nameone,nametwo):
+
+
+def findlength(name):
     
-    l1 = len(nameone)
-    l2 = len(nametwo)
+    l = len(name)
 
-    return l1,l2
+    return l
+
+
+
 
 def calculatetotal(firstsum,secondsum):
     
-    total1 = firstsum[0] + secondsum[0]
-    total2 = firstsum[1] + secondsum[1]
-
-    return total1,total2
+    total = firstsum[0] + secondsum[0] + firstsum[1] + secondsum[1]
+    
+    if total >= 100:
+        total = 100
+                                                          
+    return total
 
 
 
  
 
-firstname,secondname = getname()
-sumvowels = checkforvowels(firstname,secondname)
-sumlength = findlength(firstname,secondname)
-print(calculatetotal(sumvowels,sumlength))
+firstname = getname(input("Enter your name: ").upper())
+secondname = getname(input("Enter lovers name: ").upper())
+
+sumvowels = checkforvowels(firstname), checkforvowels(secondname)
+sumlength = findlength(firstname), findlength(secondname)
+
+print(str(calculatetotal(sumvowels,sumlength)) + "%")
 
 
 
